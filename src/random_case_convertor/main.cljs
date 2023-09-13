862d07c19e72f55e9515137d9f9b87d63a976e0f
James Crake-Merani <crake-meranij@cardiff.ac.uk>
2023-09-13 21:46:38 +0100

    Added placeholders.

--- src/random_case_convertor/main.cljs
+++ src/random_case_convertor/main.cljs
@@ -27,10 +27,11 @@
 (defn convert-to-random-case [to-convert]
   (apply str (map random-char-case to-convert)))
 
-(defn entry-box [value on-change]
+(defn entry-box [value on-change placeholder-text]
   [:textarea.textarea.textarea-bordered.flex-1.m-3
    {:on-change on-change
-    :value value} ])
+    :value value
+    :placeholder placeholder-text} ])
 
 (defn base []
   (let [to-convert (r/atom "")]
@@ -38,8 +39,8 @@
       [:div.container.mx-auto.my-4
        [:h1.text-center.text-2xl.font-bold "Random Case Converter"]
        [:div.flex.py-4.flex-col.md:flex-row
-        [entry-box @to-convert #(reset! to-convert (-> % .-target .-value))]
-        [entry-box (convert-to-random-case @to-convert) #(.showModal (.getElementById js/document "oi-modal"))]]
+        [entry-box @to-convert #(reset! to-convert (-> % .-target .-value)) "Input"]
+        [entry-box (convert-to-random-case @to-convert) #(.showModal (.getElementById js/document "oi-modal")) "Output"]]
        [:dialog {:id "oi-modal" :className "modal"}
         [:div.modal-box
          [:p.py-4 "Oi! You're not supposed to edit this box!"]]
